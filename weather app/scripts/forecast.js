5b3a3d967bda6dfe0161123790bd9b3c082e0325
Lola <elizabeth.tamera@gmail.com>
2023-01-23 08:27:28 +0100

    first commit

---
+++ weather app/scripts/forecast.js
@@ -0,0 +1,24 @@
+class Forecast{
+    constructor(){
+        this.key = '7Nug5Alqq26fPA3TBCmqd2RzuAfG6JF3';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDeets = await this.getCity(city);
+        const weather = await this.getWeather(cityDeets.Key);
+        return { cityDeets, weather };
+    }
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
